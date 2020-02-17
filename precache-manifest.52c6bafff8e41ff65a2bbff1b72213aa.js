954d9f64323886934490b4e76bb421e1711db8a1
omarezzat94 <omar.ezzat.job@gmail.com.com>
2020-02-18 01:36:29 +0200

    Updates

---
+++ precache-manifest.52c6bafff8e41ff65a2bbff1b72213aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdffd29cc84e5e333b27a50a56030f5c",
+    "url": "/recipe_app/index.html"
+  },
+  {
+    "revision": "4289197462e3f5722309",
+    "url": "/recipe_app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "17734a9bd8fea3947451",
+    "url": "/recipe_app/static/js/2.8979afd7.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/recipe_app/static/js/2.8979afd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4289197462e3f5722309",
+    "url": "/recipe_app/static/js/main.0a8049e0.chunk.js"
+  },
+  {
+    "revision": "a80d0d244b5babaf0b61",
+    "url": "/recipe_app/static/js/runtime-main.da4e96b4.js"
+  }
+]);
